65cff2703c104933e7f0edd203691a205b164601
Jordi Dosne <jordi@dosne.net>
2017-02-12 00:14:05 +0100

    Add production file to test bundle size

---
+++ fuse.prod.js
@@ -0,0 +1,31 @@
+const fsbx = require("fuse-box");
+
+const dotenv = require('dotenv')
+const env = dotenv.config().parsed // will return an object
+
+// Create FuseBox Instance
+fsbx.FuseBox.init({
+    homeDir: "src/",
+        log: true,
+        cache: false,
+    outFile: "./build/bundle.prod.js",
+    plugins: [
+        [
+            fsbx.SassPlugin(),
+            fsbx.CSSResourcePlugin({ inline: true }),
+            fsbx.CSSPlugin()
+        ],
+        fsbx.EnvPlugin(env),
+        fsbx.BabelPlugin({
+            config: {
+                sourceMaps: true,
+                presets: [["es2015", { "loose": true }], "react"],
+                plugins: [
+                    "react-hot-loader/babel",
+                    "transform-class-properties"
+                ]
+            }
+        }),
+        fsbx.UglifyJSPlugin()
+    ]
+}).bundle("> index.js");
