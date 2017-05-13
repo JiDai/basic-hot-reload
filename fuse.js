cb5c5e932bbf85b24876b2f390b1c54bb660c3df
Jordi Dosne <jordi@dosne.net>
2017-05-13 08:10:30 +0200

    Doc, fix routing

--- fuse.js
+++ fuse.js
@@ -1,30 +1,53 @@
 const fsbx = require("fuse-box");
+const path = require("path");
+const express = require("express");
+const eslinter = require("fuse-box-eslint-plugin");
 
+// Récupération de la config d'environnement via DotEnv
 const dotenv = require('dotenv')
 const env = dotenv.config().parsed // will return an object
-const eslinter = require("fuse-box-eslint-plugin");
 
 // Create FuseBox Instance
 const fuseBox = new fsbx.FuseBox({
+    // Code base root path of your app
     homeDir: "src/",
-    sourcemaps: true,
+    // All JS will be concat in this file
     outFile: "./public/build/bundle.js",
+    // Configure source maps
+    sourcemaps: true,
+    // Plugins to handle different types of file to bundle
+    // Files are found by extension by the plugin itself
+    // Ex: fsbx.SassPlugin() will find .scss files
     plugins: [
+        // SCSS plugin to generate inline styles
+        // Here, we have an array to chain plugins
         [
             fsbx.SassPlugin(),
-            fsbx.CSSResourcePlugin({ inline: true }),
+            fsbx.CSSResourcePlugin({inline: true}),
             fsbx.CSSPlugin()
         ],
+        // Injecting environment vars to be readable in browser
         fsbx.EnvPlugin(env),
+        // Handle JS files
+        // Here, we have an array to chain plugins
         [
+            // Lint all the things
             eslinter({
                 pattern: /js(x)*$/,
             }),
+            // Babel used for compile jsx files
             fsbx.BabelPlugin(),
         ]
     ]
 });
 
-fuseBox.devServer("> index.js", {
+// Launch the dev server
+const server = fuseBox.devServer("> index.js", {
     root: './public/'
+});
+// Declare all static file to be served
+server.httpServer.app.use(express.static(path.join('public')));
+// Fallback to index.html for all routes (and wil be handled by React-router)
+server.httpServer.app.get('*', function (req, res) {
+    res.sendFile(path.resolve('public', 'index.html'));
 });
